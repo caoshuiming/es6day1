306c8481cc7033b82e6e4b30e8d25ad8b8735e54
caoshuiming <2639067440@qq.com>
2021-01-15 11:37:34 +0800

    update 20210115morning

--- index.js
+++ index.js
@@ -154,8 +154,8 @@
 // console.log(arr);
 
 // filter()数组去重?(过滤!)
-var myArr = [1,3,4,5,6,3,7,4];
-console.log(myArr.filter((value,index,arr)=>arr.indexOf(value)===index));
+// var myArr = [1,3,4,5,6,3,7,4];
+// console.log(myArr.filter((value,index,arr)=>arr.indexOf(value)===index));
 
 // let json = {
 //     '0':'csm',
@@ -263,6 +263,194 @@ console.log(myArr.filter((value,index,arr)=>arr.indexOf(value)===index));
 // console.log(arr.map(x => 'web'));
 
 // 数组转成字符串
-let arr = ['csm','曹水明','fw'];
-console.log(arr.toString());
-console.log(arr.join('|'));
+// let arr = ['csm','曹水明','fw'];
+// console.log(arr.toString());
+// console.log(arr.join('|'));
+
+// 对象
+//对象赋值
+// let name = 'csm';
+// let skill = 'web';
+// var obj = {name,skill};
+// console.log(obj);
+
+// key值的构建
+// let key = 'skill';
+// var obj = {
+//     [key]:'web'
+// }
+// console.log(obj.skill); // 输出web
+
+// 自定义对象方法
+// let obj = {
+//     add:function(a,b){
+//         return a+b;
+//     }
+// }
+// console.log(obj.add(1,2))
+
+// ES6新增
+// is方法
+// let obj1 = {name:'csm'};
+// let obj2 = {name:'csm'};
+// console.log(obj1.name===obj2.name);
+// console.log(Object.is(obj1.name,obj2.name));
+// // ===同值相等 is严格相等
+// console.log(+0===-0);
+// console.log(NaN===NaN);
+// console.log(Object.is(+0,-0));
+// console.log(Object.is(NaN,NaN));
+
+// assign方法
+// let a = {a:'csm'};
+// let b = {b:'曹水明'};
+// let c = {c:'fw'};
+// let d = Object.assign(a,b,c);
+// console.log(d);
+
+// Symbol
+// 之前的
+// let a = new String;
+// let b = new Number;
+// let c = new Boolean;
+// let d = new Array;
+// let e = new Object;
+// ES6的
+// let f = Symbol();
+// console.log(typeof(f));
+
+// Symbol的打印
+// let csm = Symbol('曹水明');
+// console.log(csm);
+// console.log(csm.toString());
+
+// Symbol在对象中的应用
+// let csm = Symbol();
+// let obj = {
+//     [csm]:'曹水明'
+// }
+// console.log(obj[csm]);
+// console.log(obj.csm); // 输出undefined
+// obj[csm] = 'fw';
+// console.log(obj[csm])
+
+// Symbol对象元素的保护作用
+// let obj = {
+//     name:'csm',
+//     skill:'web',
+//     age:'22'
+// };
+// for(let item in obj){
+//     console.log(obj[item]);
+// }
+// let obj = {
+//     name:'csm',
+//     skill:'web'
+// };
+// let age = Symbol();
+// obj[age]=18;
+// console.log(obj);
+// for(let item in obj){
+//     console.log(obj[item]);
+// }
+// console.log(obj[age]);
+
+// set的声明
+// set 和 数组 的区别:set不允许有重复的值，如果有只显示一个，Set不是数组 
+// let setArr = new Set(['csm','曹水明','web','csm',]);
+// setArr.add('前端技术');
+// console.log(setArr);
+// has查找set中的中
+// console.log(setArr.has('csm'));
+// set值的增删改
+// setArr.delete('web');
+// console.log(setArr);
+// 全部删除
+// setArr.clear();
+
+//循环输出
+// for...of
+// for(let item of setArr){
+//     console.log(item);
+// }
+// forEach
+// setArr.forEach((value) => console.log(value));
+// size属性
+// console.log(setArr.size);
+
+// 去重
+// const s = new Set();
+// [2,3,4,5,5,2,4,2].forEach(x => s.add(x));
+// for(let i of s){
+//     console.log(i);
+// }
+
+// weakSet的声明 传入的值必须是对象
+// let weakObj = new WeakSet();
+// let obj = {a:'csm',b:'曹水明'};
+// // let obj1 = obj; //这样只会输出一遍
+// weakObj.add(obj);
+// let obj1 = {a:'csm',b:'曹水明'}; //这样会输出两遍
+// weakObj.add(obj1);
+// console.log(weakObj);
+
+// map数据类型
+// json
+// let json = {
+//     name:'csm',
+//     skill:'web'
+// };
+// console.log(json.name);
+
+// map更灵活,比较随便
+// var map = new Map();
+// map.set(json,'I am');
+// map.set('I am',json);
+// console.log(map);
+
+// map的增删查
+// get取值
+// console.log(map.get(json));
+// delete 删除特定的值
+// map.delete(json);
+// console.log(map);
+// clear删除全部的值
+// map.clear();
+// size
+// console.log(map.size);
+// has
+// console.log(map.has(json));
+
+// Map 转为数组
+const myMap = new Map()
+  .set(true, 7)
+  .set({foo: 3}, ['abc']);
+const arr = [...myMap]
+console.log(arr)
+// [ [ true, 7 ], [ { foo: 3 }, [ 'abc' ] ] ]
+
+// 数组 转为 Map
+new Map([
+    [true, 7],
+    [{foo: 3}, ['abc']]
+])
+// Map {
+//   true => 7,
+//   Object {foo: 3} => ['abc']
+// }
+
+// Map 转为对象
+// function strMapToObj(strMap) {
+// let obj = Object.create(null);
+// for (let [k,v] of strMap) {
+//     obj[k] = v;
+// }
+// return obj;
+// }
+// const myMap = new Map().set('yes', true).set('no', false);
+// strMapToObj(myMap)
+// // { yes: true, no: false }
+
+// // 对象转为 Map
+// let obj = {"a":1, "b":2};
+// let map = new Map(Object.entries(obj));
